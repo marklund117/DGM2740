98a8e8539e3bf1048c87a18a74d2f3f08964d013
Mark Lund <marklund117@gmail.com>
2022-04-16 17:38:41 -0600

    nav starting

--- band/scripts/main.js
+++ band/scripts/main.js
@@ -1,10 +1,8 @@
 // main JS script for 2740 module 4
 function toggleMenu(){
-    document.getElementById("smallnav").classList.toggle("open")
-    document.getElementById("burger").classList.toggle("open")
-
+    document.getElementById("primaryNav").classList.toggle("open")
 }
 
-const donald = document.getElementById("burger")
+const x = document.getElementById("burger")
 
-donald.onclick = toggleMenu // toggle menu when burger is clicked
+x.onclick = toggleMenu // toggle menu when burger is clicked
