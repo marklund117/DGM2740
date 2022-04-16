779e6f3df7572e170dcc0a018b03563ff3d79f6e
Mark Lund <marklund117@gmail.com>
2022-04-16 13:27:07 -0600

    getting band rolling

---
+++ band/scripts/main.js
@@ -0,0 +1,10 @@
+// main JS script for 2740 module 4
+function toggleMenu(){
+    document.getElementById("smallnav").classList.toggle("open")
+    document.getElementById("burger").classList.toggle("open")
+
+}
+
+const donald = document.getElementById("burger")
+
+donald.onclick = toggleMenu // toggle menu when burger is clicked
