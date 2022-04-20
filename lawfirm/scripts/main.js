8c516314b37fc6fad60cb565f111d734f5de46ab
Mark Lund <marklund117@gmail.com>
2022-04-20 00:01:32 -0600

    I think im done here

--- lawfirm/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// main JS script for 2740 module 4
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open")
-}
-
-const x = document.getElementById("burger")
-
-x.onclick = toggleMenu // toggle menu when burger is clicked
