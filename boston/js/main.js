f896abff73ec49a00eb1d78564ea729f3a843998
Mark Lund <marklund117@gmail.com>
2022-04-22 23:29:55 -0600

    yes

--- boston/js/main.js
+++ boston/js/main.js
@@ -2,10 +2,9 @@
 
 const hb = document.querySelector('#hambutton')
 
-const pn = document.querySelector('.navlist')
+const pn = document.querySelector('#primaryNav')
 
 hb.addEventListener('click', () => {
-    hb.classList.toggle('open')
     pn.classList.toggle('open')
 })
 
