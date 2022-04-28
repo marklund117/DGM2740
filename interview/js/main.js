6da140369178b5190292daa5e360860153e1db42
Mark Lund <marklund117@gmail.com>
2022-04-27 18:44:09 -0600

    yeah i think the nav works

---
+++ interview/js/main.js
@@ -0,0 +1,8 @@
+// main JS script for 2740 module 15
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open")
+}
+
+const x = document.getElementById("burger")
+
+x.onclick = toggleMenu // toggle menu when burger is clicked
